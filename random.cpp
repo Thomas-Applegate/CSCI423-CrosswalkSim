6154c4df5450d4d3404338456753ab1001538142
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2023-11-02 14:19:54 -0600

    complete random class

--- random.cpp
+++ random.cpp
@@ -1,4 +1,6 @@
 #include "random.h"
+#include <stdexcept>
+#include <cmath>
 
 random::random() : m_prng(), m_rfile()
 {
@@ -32,4 +34,35 @@ void random::use_file(const std::string& s)
 {
 	m_rfile.close();
 	m_rfile.open(s);
+}
+
+double random::uniform(double a, double b)
+{
+	if(a >= b) throw std::invalid_argument("a must be less than b");
+	return a + (b-1) * (*this)();
+}
+
+long random::equilikely(long a, long b)
+{
+	if(a >= b) throw std::invalid_argument("a must be less than b");
+	return a + (long)((b-a+1) * (*this)());
+}
+
+double random::exponential(double mu)
+{
+	if(mu <= 0.0) throw std::domain_error("mu must be greater than 0");
+	return -mu * std::log(1.0 - (*this)());
+}
+
+
+long random::geometric(double p)
+{
+	if(p >= 1.0 || p <= 0.0) throw std::domain_error("p must be greater than 0 and less than 1");
+	return (long)(log(1- (*this)())/log(p));
+}
+
+bool random::bernouli(double p)
+{
+	if(p > 1.0 || p < 0.0) throw std::domain_error("p must be between 0 and 1");
+	return (*this)() <= p ? true : false;
 }
--- random.h
+++ random.h
@@ -12,6 +12,12 @@ public:
 	
 	void use_prng();
 	void use_file(const std::string& s);
+	
+	double uniform(double a, double b);
+	long equilikely(long a, long b);
+	double exponential(double mu);
+	long geometric(double p);
+	bool bernouli(double p);
 private:
 	std::random_device m_prng;
 	std::ifstream m_rfile;
