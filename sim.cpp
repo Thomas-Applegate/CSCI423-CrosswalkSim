5db2cd80a1bb4b364228a73950378e80b65d1aff
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2023-11-06 12:21:39 -0700

    begin simulation

--- sim.cpp
+++ sim.cpp
@@ -1,4 +1,5 @@
 #include "sim.h"
+#include "event.h"
 
 #ifdef DEBUG
 simulator::simulator() : m_event_list(), m_auto_random(), m_ped_random(),
@@ -12,7 +13,54 @@ simulator::simulator(const std::string& auto_random,
 
 void simulator::run(unsigned int N)
 {
-	//TODO
+	unsigned int N_auto = N - 1;
+	unsigned int N_ped = N -1;
+	m_event_list.emplace(m_ped_random.exponential(6), event::Type::ped_arrival,
+		m_ped_random.uniform(2.6, 4.1));
+	m_event_list.emplace(m_auto_random.exponential(8), event::Type::auto_arrival,
+		m_auto_random.uniform(25, 35));
+	
+	while(!m_event_list.empty())
+	{
+		auto current_event = m_event_list.begin();
+		m_clock = current_event->m_at;
+		
+		switch(current_event->m_type)
+		{
+			case event::Type::auto_arrival:
+				if(N_auto > 0)
+				{
+					m_event_list.emplace(m_clock + m_auto_random.exponential(8),
+						event::Type::auto_arrival, m_auto_random.uniform(25, 35));
+					N_auto--;
+				}
+				break;
+			case event::Type::ped_arrival:
+				if(N_ped > 0)
+				{
+					m_event_list.emplace(m_clock + m_ped_random.exponential(6),
+						event::Type::ped_arrival, m_ped_random.uniform(2.6, 4.1));
+					N_ped--;
+				}
+				break;
+			case event::Type::ped_at_button:
+				break;
+			case event::Type::ped_impatient:
+				break;
+			case event::Type::green_expires:
+				break;
+			case event::Type::yellow_expires:
+				break;
+			case event::Type::red_expires:
+				break;
+			case event::Type::auto_exit:
+				break;
+			case event::Type::ped_exit:
+				break;
+		}
+		
+		m_event_list.erase(current_event);
+	}
 }
 
 double simulator::clock() const { return m_clock; }
