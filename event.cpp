d1128d98fddba099469ab144d93530dd6a651086
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2023-11-06 10:47:32 -0700

    skeleton event class

--- event.cpp
+++ event.cpp
@@ -1,1 +1,9 @@
 #include "event.h"
+
+event::event(double at) noexcept : m_at(at) {}
+double event::at() const { return m_at; }
+
+bool operator<(const event& a, const event& b)
+{
+	return a.m_at < b.m_at;
+}
--- event.h
+++ event.h
@@ -2,5 +2,12 @@
 
 class event
 {
+public:
+	event(double at) noexcept;
 	
+	double at() const;
+	
+	friend bool operator<(const event& a, const event& b);
+private:
+	double m_at;
 };
